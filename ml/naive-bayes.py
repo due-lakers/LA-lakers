35105e632424c9aced2cbb244d11869061ed747f
shahab-ab <shahab.ab@outlook.com>
2021-01-22 04:57:51 +0100

    SVM accuracy highly improved, algorithm examined

--- ml/naive-bayes.py
+++ ml/naive-bayes.py
@@ -1,15 +1,13 @@
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-#from sklearn.metrics import classification_report
-#from sklearn.metrics import confusion_matrix
 
-stu = pd.read_csv('D:\shahabab\DevCenter\LA-lakers\data\edu-dataset.csv')
+stud_df = pd.read_csv('../data/edu-dataset.csv')
 
 #dataset split
-x = stu.drop('Class',axis = 1)
-x = x.drop('SectionID',axis = 1)
-y = stu['Class']
+x = stud_df.drop('Class', axis = 1)
+x = x.drop('SectionID', axis = 1)
+y = stud_df['Class']
 x = pd.get_dummies(x)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 10)
@@ -19,4 +17,4 @@ nb =  GaussianNB()
 nb.fit(x_train, y_train)
 y_pred = nb.predict(x_test)
 
-print("Accuracy of naive bayes algorithm: ", nb.score(x_test,y_test))
+print("\n Accuracy of naive bayes algorithm: ", nb.score(x_test,y_test))
--- ml/svm.py
+++ ml/svm.py
@@ -1,40 +1,90 @@
-# read dataset
+# The goal is to test our dataset with both simple SVM and Kernel-SVM (and its variants) in order for us to be able to choose one based on the results.
+
+# import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import seaborn as sns
-#%matplotlib inline
 
-#stu=pd.read_csv('edu-dataset.csv')
-bankdata = pd.read_csv("bill_authentication.csv")
+# read dataset
+stud_df = pd.read_csv('../data/edu-dataset.csv')
 
-# exploratory data analysis
-#stu.shape
-#stu.head()
-bankdata.shape
-bankdata.head()
+# pre-process data
+# (1) split the data into attributes and labels
+x = stud_df.drop('Class', axis=1)
+x = x.drop('SectionID', axis=1)
+y = stud_df['Class']
+x = pd.get_dummies(x)
 
-# process data
-# (1) dividing the data into attributes and labels and
-X = bankdata.drop('Class', axis = 1)
-y = bankdata['Class']
-
-# (2) dividing the data into training and testing sets
+# (2) split the data into train and test sets
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=10) 
+# random-state=10 had best results! 
 
-# train the model
+# train the algorithm
+# (1) simple SVM
 from sklearn.svm import SVC
-svclassifier = SVC(kernel = 'linear')
-svclassifier.fit(X_train, y_train)
+svclassifier = SVC(random_state=10)
+svclassifier.fit(x_train,y_train)
+print('\n Simple SVM accuracy with *random-state 10: ', svclassifier.score(x_test, y_test))
 
 # make predictions
-y_pred = svclassifier.predict(X_test)
+y_pred = svclassifier.predict(x_test)
 
-# evaluate the algorithm
+# evaluate the algorithm with some metrics
 from sklearn.metrics import classification_report, confusion_matrix
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
-from sklearn.metrics import classification_report, confusion_matrix
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+print('\n', confusion_matrix(y_test, y_pred))
+print('\n', classification_report(y_test, y_pred))
+
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(x_train, y_train)
+print('\n Simple SVM accuracy *linear:  ', svclassifier.score(x_test, y_test))
+
+# make predictions
+y_pred = svclassifier.predict(x_test)
+
+# evaluate the algorithm with some metrics
+print('\n', confusion_matrix(y_test, y_pred))
+print('\n', classification_report(y_test, y_pred))
+
+
+# (2) Kernel-SVM
+# (2.1) Polynominal Kernel
+svclassifier = SVC(kernel='poly', degree=8)
+svclassifier.fit(x_train, y_train)
+
+print('\n Kernel SVM accuracy with *Polynominal kernel:  ', svclassifier.score(x_test, y_test))
+
+# make predictions
+y_pred = svclassifier.predict(x_test)
+
+# evaluate the algorithm with some metrics
+print('\n', confusion_matrix(y_test, y_pred))
+print('\n', classification_report(y_test, y_pred))
+
+# (2.2) Gaussian Kernel
+svclassifier = SVC(kernel='rbf')
+svclassifier.fit(x_train, y_train)
+
+print('\n Kernel SVM accuracy with *Gaussian kernel:  ', svclassifier.score(x_test, y_test))
+
+# make predictions
+y_pred = svclassifier.predict(x_test)
+
+# evaluate the algorithm with some metrics
+print('\n', confusion_matrix(y_test, y_pred))
+print('\n', classification_report(y_test, y_pred))
+
+# (2.3) Sigmoid Kernel
+svclassifier = SVC(kernel='sigmoid')
+svclassifier.fit(x_train, y_train)
+
+print('\n Kernel SVM accuracy with *Sigmoid kernel:  ', svclassifier.score(x_test, y_test))
+
+# make predictions
+y_pred = svclassifier.predict(x_test)
+
+# evaluate the algorithm with some metrics
+print('\n', confusion_matrix(y_test, y_pred))
+print('\n', classification_report(y_test, y_pred))
+
+print('\n\n Decision is ours!')
