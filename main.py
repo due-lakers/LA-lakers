36117867892158c03ec9faead88f0aaf0128b7f5
Chris <495032732@qq.com>
2021-01-24 20:26:22 +0100

    formUpdate

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, url_for
 import pickle
 import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static')
 app.debug = True
 
 
@@ -11,20 +11,44 @@ app.debug = True
 def hello_world():
     f2 = open('model/svm.model', 'rb')
     s2 = f2.read()
-    l1 = [10., 17., 12., 40., 1., 1.] # H
-    l2 = [70., 82., 3., 73., 0. ,0.] #M
-    l3 = [30., 52., 23., 33., 0., 1.] #M
+    l1 = [10., 17., 12., 40., 1., 1.]  # H
+    l2 = [70., 82., 3., 73., 0., 0.]  # M
+    l3 = [30., 52., 23., 33., 0., 1.]  # M
 
-    model1 = pickle.loads(s2)
+    # model1 = pickle.loads(s2)
     # expected = test_y
-    predicted = model1.predict(np.array(l1).reshape(1,-1))
-    return str(predicted)
+    # predicted = model1.predict(np.array(l1).reshape(1, -1))
+    # return str(predicted)
+    return render_template('index.html')
 
 
+@app.route('/index')
+def student():
+    return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('index.html', title='Machine Learning')
+
+@app.route('/form')
+def formPage():
+    return render_template('form_controls.html', title='Machine Learning')
+
+
+# @app.route('/formTest',methods=['POST', 'GET'])
+# def form():
+#     if request.method=='POST':
+#         print(request.form.get("test1"))
+#         re=request.form.get("test1")
+#
+#
+#
+#     return render_template('form_controls.html', title='Machine Learning',results=re)
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        return render_template("form_controls.html", result=result)
 
 
 if __name__ == '__main__':
---
+++ templates/precache-manifest.2703946fdea595695321b0a44c88c545.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51747e1a5c472e0556e36a3d754ba37a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f996e40e59c8ba2b2c45",
+    "url": "./static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "85343a71cc4982a446a0",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f996e40e59c8ba2b2c45",
+    "url": "./static/js/2.2a3c24d5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "../static/js/2.2a3c24d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed7932d1b036274b35c",
+    "url": "./static/js/3.9d7df8a5.chunk.js"
+  },
+  {
+    "revision": "85343a71cc4982a446a0",
+    "url": "./static/js/main.c72c680e.chunk.js"
+  },
+  {
+    "revision": "9c3cc3c440bb988dcbdb",
+    "url": "./static/js/runtime-main.73d95b88.js"
+  }
+]);
